bb88201a602f3202ddb57f5a9851875befac9807
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-08-21 20:30:22 +0200

    created a patient class which just holds patient information, as well as all the getters and setters for each piece of patient information

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Patient {
     private String patientID;
     private String firstName;
@@ -5,5 +7,74 @@ public class Patient {
     private int age;
     private String gender;
     private String medicalCondition;
-    private String PatientCatagory;
+    private PatientCategory patientCategory;
+}
+public Patient(String patientID, String firstName, String lastName, int age, String gender, String medicalCondition, PatientCategory patientCategory) {
+    this.patientID = patientID;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+    this.medicalCondition = medicalCondition;
+    this.patientCategory = patientCategory;
+}
+////////////////////////////////////////////////////////////////
+public String getPatientID() {
+    return patientID;
+}
+public void setPatientID(String patientID) {
+    this.patientID = patientID;
+}
+//////////////////////////////////////////////////////////////
+public String getFirstName() {
+    return firstName;
+}
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
+///////////////////////////////////////////////////////////
+public String getLastName() {
+    return lastName;
+}
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+////////////////////////////////////////////////////////////
+public int getAge() {
+    return age;
+}
+public void setAge(int age) {
+    this.age = age;
+}
+//////////////////////////////////////////////////////////
+public String getGender() {
+    return gender;
+}
+public void setGender(String gender) {
+    this.gender = gender;
+}
+///////////////////////////////////////////////////////////
+public String getMedicalCondition() {
+    return medicalCondition;
+}
+public void setMedicalCondition(String medicalCondition) {
+    this.medicalCondition = medicalCondition;
+}
+/////////////////////////////////////////////////////////////
+public PatientCategory getPatientCategory() {
+    return patientCategory;
+}
+public void setPatientCategory(PatientCategory patientCategory) {
+    this.patientCategory = patientCategory;
+}
+///////////////////////////////////////////////////////////////
+
+public void displayDetails() {
+    System.out.println("ID: " + patientID);
+    System.out.println("Name: " + firstName);
+    System.out.println("Surname: " + lastName);
+    System.out.println("Age: " + age);
+    System.out.println("Gender: " + gender);
+    System.out.println("Medical Condition: " + medicalCondition);
+    System.out.println("Category: " + patientCategory);
 }
