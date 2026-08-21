3cee2f43a130ec7712c04b8e14138523b11eb45e
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-08-21 17:28:03 +0200

    Created patient catagory enum and patient class and declared patient variables

---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+    private String patientID;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private String medicalCondition;
+    private String PatientCatagory;
+}
