32fbbe657b73a5e45fe61b0f6a46bd756730b059
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-08-23 17:22:14 +0200

    Added switch case for entering patient information, exiting menu and invalid choice message

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,35 @@ public class Main {
             System.out.println("2. Search for a patient");
             System.out.println("0. Exit");
             System.out.println("Please choose an option: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter patient ID: ");
+                    String id  = scanner.nextLine();
+                    System.out.println("Enter patient name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.println("Enter patient last name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.println("Enter patient gender: ");
+                    String gender = scanner.nextLine();
+                    System.out.println("Enter patient age: ");
+                    int age = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Enter patient medical condition: ");
+                    String medicalCondition = scanner.nextLine();
+                    break;
+                case 2:
+                    //
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Good Bye!");
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again.");
+            }
+
         }
 
     }
